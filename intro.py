dc8bd16293bf0fb7e37bf3b391575f3ebc134836
srushhhh <acknowledge2312@gmail.com>
2026-09-06 23:17:42 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,6 @@
+# 1. Ask the user for their name
+user_name=input("What is your name?")
+# 2. Ask the user for their favorite programming language
+fav_lang=input("Which is your favorite programming language?")
+# 3.print a message
+print("Wow,Great!")
